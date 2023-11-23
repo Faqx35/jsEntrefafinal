1a30918bd223feed6430802a68ba0a23fd85be77
Facundo Miguel Malica <facundo.malica@gmail.com>
2023-11-23 17:42:08 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const estudiantes = [];
+    const resultadosDiv = document.getElementById('resultados');
+    const resultadoEstudianteDiv = document.getElementById('resultadoEstudiante');
+
+    document.getElementById('agregar').addEventListener('click', () => {
+        const nombre = document.getElementById('nombre').value;
+        const apellido = document.getElementById('apellido').value;
+        const nota1 = parseFloat(document.getElementById('nota1').value);
+        const nota2 = parseFloat(document.getElementById('nota2').value);
+        const nota3 = parseFloat(document.getElementById('nota3').value);
+
+        if (nombre && apellido && !isNaN(nota1) && !isNaN(nota2) && !isNaN(nota3)) {
+            const promedio = (nota1 + nota2 + nota3) / 3;
+            estudiantes.push({ nombre, apellido, notas: [nota1, nota2, nota3], promedio });
+            guardarEnLocalStorage(estudiantes);
+            limpiarFormulario();
+        } else {
+            alert('Ingresa todos los campos correctamente');
+        }
+    });
+
+    document.getElementById('buscarEstudiante').addEventListener('click', () => {
+        const apellidoABuscar = document.getElementById('buscar').value;
+        const resultado = estudiantes.find(estudiante => estudiante.apellido === apellidoABuscar);
+
+        if (resultado) {
+            resultadosDiv.innerHTML = `
+                <p>Nombre: ${resultado.nombre} ${resultado.apellido}</p>
+                <p>Notas: ${resultado.notas.join(', ')}</p>
+                <p>Promedio: ${resultado.promedio}</p>
+            `;
+            resultadoEstudianteDiv.style.display = 'block';
+
+        
+            Swal.fire({
+                icon: 'success',
+                title: '¡Estudiante encontrado!',
+                text: `Nombre: ${resultado.nombre} ${resultado.apellido}\nPromedio: ${resultado.promedio}`,
+            });
+        } else {
+            resultadosDiv.innerHTML = 'Estudiante no encontrado';
+            resultadoEstudianteDiv.style.display = 'none';
+
+            
+            Swal.fire({
+                icon: 'error',
+                title: '¡Estudiante no encontrado!',
+                text: 'No se encontró ningún estudiante con ese apellido.',
+            });
+        }
+    });
+
+    function guardarEnLocalStorage(estudiantes) {
+        localStorage.setItem('estudiantes', JSON.stringify(estudiantes));
+    }
+
+    function limpiarFormulario() {
+        document.getElementById('nombre').value = '';
+        document.getElementById('apellido').value = '';
+        document.getElementById('nota1').value = '';
+        document.getElementById('nota2').value = '';
+        document.getElementById('nota3').value = '';
+    }
+
+    const estudiantesGuardados = JSON.parse(localStorage.getItem('estudiantes')) || [];
+    estudiantes.push(...estudiantesGuardados);
+});
